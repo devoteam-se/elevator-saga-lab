daeb9e83ee2b503a6ca55793bd256ecc5430d958
Filip Bontin <filip.bontin@devoteam.com>
2023-02-10 09:48:28 +0100

    Level 3

--- elevatorsaga.js
+++ elevatorsaga.js
@@ -1,35 +1,31 @@
 ({
     init:
-    /**
-     * @param {Elevator[]} elevators - the elevators
-     * @param {Floor[]} floors - the floors
-     */
-    function init(elevators, floors) {
-        let goingUp = true;
-        // Let's go through all the elevators
-        for (const elevator of elevators) {
-            // Whenever the elevator is idle, make it go to all the floors
-            elevator.on('idle', () => {
-                const curr = elevator.currentFloor(); 
-                if (elevator.currentFloor() === floors.length) {
-                    goingUp = false;
-                } 
-                if (elevator.currentFloor() === 0) {
-                    goingUp = true;
-                } 
+        /**
+         * @param {Elevator[]} elevators - the elevators
+         * @param {Floor[]} floors - the floors
+         */
+        function init(elevators, floors) {
+            // Let's go through all the elevators
+            for (const elevator of elevators) {
+                // Whenever the elevator is idle, make it go to all the floors
+                elevator.on('idle', () => {
+                    if (elevator.getPressedFloors().length) {
+                        elevator.getPressedFloors().forEach(f => elevator.goToFloor(f));
+                    } else {
+                        elevator.goToFloor(Math.floor(Math.random() * floors.length));
+                    }
 
-                elevator.goToFloor((curr + (goingUp ? 1 : -1)) % floors.length);
-            })            
+                })
+            }
+        },
+
+    update:
+        /**
+         * @param {number} dt - time difference from last call
+         * @param {Elevator[]} elevators - the elevators
+         * @param {Floor[]} floors - the floors
+         */
+        function update(dt, elevators, floors) {
+            // We normally don't need to do anything here
         }
-    },
-
-    update: 
-    /**
-     * @param {number} dt - time difference from last call
-     * @param {Elevator[]} elevators - the elevators
-     * @param {Floor[]} floors - the floors
-     */
-    function update(dt, elevators, floors) {
-        // We normally don't need to do anything here
-    }
 })
