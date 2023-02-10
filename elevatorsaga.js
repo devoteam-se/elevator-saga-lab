85f4fe089e25eb67ac059f088e5fe7d3da2e33a1
Fredrik Jönsson <fredrik.jonsson@devoteam.com>
2023-02-10 13:59:13 +0100

    challenge13

--- elevatorsaga.js
+++ elevatorsaga.js
@@ -5,6 +5,32 @@
      * @param {Floor[]} floors - the floors
      */
     function init(elevators, floors) {
+      /**
+       *
+       * @param {Elevator} elevator
+       * @param {number} floorNum
+       */
+      const removeNextFloorFromQueueIfIrrelevant = (elevator, floorNum) => {
+        const nextFloorNum = elevator.destinationQueue.length
+          ? elevator.destinationQueue[0]
+          : -1;
+        if (nextFloorNum !== -1) {
+          if (elevator.getPressedFloors().includes(nextFloorNum)) {
+            return;
+          }
+          const nextFloor = floors.find((f) => f.floorNum() === nextFloorNum);
+          if (
+            nextFloor.buttonStates.down === "" &&
+            nextFloor.buttonStates.up === ""
+          ) {
+            elevator.destinationQueue = elevator.destinationQueue.filter(
+              (q) => q !== floorNum
+            );
+            elevator.checkDestinationQueue();
+          }
+        }
+      };
+
       // Let's go through all the elevators
       for (const elevator of elevators) {
         // Whenever the elevator is idle, make it go to all the floors
@@ -17,6 +43,11 @@
         });
 
         elevator.on("passing_floor", function (floorNum, direction) {
+          //   const slice = elevator.getPressedFloors().slice(0, 3);
+          //   if (!slice.includes(floorNum)) {
+          //     return;
+          //   }
+
           if (elevator.destinationQueue.includes(floorNum)) {
             elevator.destinationQueue = elevator.destinationQueue.filter(
               (q) => q !== floorNum
@@ -27,12 +58,36 @@
         });
 
         elevator.on("stopped_at_floor", function (floorNum) {
-          if (elevator.destinationQueue.includes(floorNum)) {
-            elevator.destinationQueue = elevator.destinationQueue.filter(
-              (q) => q !== floorNum
-            );
-            elevator.checkDestinationQueue();
-          }
+          removeNextFloorFromQueueIfIrrelevant(elevator, floorNum);
+
+          //   if (elevator.destinationQueue.includes(floorNum)) {
+          //     elevator.destinationQueue = elevator.destinationQueue.filter(
+          //       (q) => q !== floorNum
+          //     );
+          //     elevator.checkDestinationQueue();
+          //   }
+          //
+          //
+          //   // Kolla om naesta floor fortfarande maste besoekas.
+          //   const nextFloorNum = elevator.destinationQueue.length
+          //     ? elevator.destinationQueue[0]
+          //     : -1;
+          //   if (nextFloorNum !== -1) {
+          //     if (elevator.getPressedFloors().includes(nextFloorNum)) {
+          //       return;
+          //     }
+          //     const nextFloor = floors.find((f) => f.floorNum() === nextFloorNum);
+          //     if (
+          //       nextFloor.buttonStates.down === "" &&
+          //       nextFloor.buttonStates.up === ""
+          //     ) {
+          //       return;
+          //     }
+          //     elevator.destinationQueue = elevator.destinationQueue.filter(
+          //       (q) => q !== nextFloorNum
+          //     );
+          //     elevator.checkDestinationQueue();
+          //   }
         });
       }
 
@@ -42,10 +97,20 @@
        */
       const onButtonPressed = (floor) => {
         if (
-          elevators.some((e) => e.destinationQueue.includes(floor.floorNum()))
+          elevators
+            .map((e) => e.destinationQueue.slice(0, 2))
+            .some((q) => q.includes(floor.floorNum()))
         ) {
           return;
         }
+        // if (
+        //   elevators
+        //     .filter((e) => e.destinationQueue.length)
+        //     .some((e) => e.destinationQueue[0] === floor.floorNum())
+        // ) {
+        //   return;
+        // }
+
         const freeElevators = elevators.filter(
           (e) => e.destinationQueue.length === 0
         );
@@ -54,8 +119,14 @@
         } else {
           const leastBusy = elevators.sort(
             (e1, e2) => e1.destinationQueue.length - e2.destinationQueue.length
+          )[0];
+          const allLeastBusy = elevators.filter(
+            (e) =>
+              e.destinationQueue.length === leastBusy.destinationQueue.length
           );
-          leastBusy[0].goToFloor(floor.floorNum());
+          allLeastBusy[
+            Math.floor(Math.random() * allLeastBusy.length)
+          ].goToFloor(floor.floorNum());
         }
       };
 
