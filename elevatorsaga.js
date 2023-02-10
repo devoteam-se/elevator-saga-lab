a46780c912453eaafc5b741a4bae16cf29eaa587
Kim Johnsson <kim.johnsson-ext@alstomgroup.com>
2023-02-10 10:44:33 +0100

    kim

--- elevatorsaga.js
+++ elevatorsaga.js
@@ -5,15 +5,48 @@
      * @param {Floor[]} floors - the floors
      */
     function init(elevators, floors) {
-        // Let's go through all the elevators
-        for (const elevator of elevators) {
-            // Whenever the elevator is idle, make it go to all the floors
-            elevator.on('idle', () => {
-                elevator.goToFloor(0);
-                elevator.goToFloor(1);
-                elevator.goToFloor(2);
-            })            
+
+        // Add floor to elevator with smallest que if the floor isn't already in que
+        const callElevator = (floor) => {
+            const elevatorWithSmallestCue = elevators.reduce((a, b) => a.destinationQueue.length <= b.destinationQueue.length ? a : b)
+            const pendingFloors = elevators.flatMap(elevator => elevator.destinationQueue)
+       
+            if (!pendingFloors.includes(floor.level)) {
+                elevatorWithSmallestCue.destinationQueue.push(floor.level)
+                elevatorWithSmallestCue.checkDestinationQueue();
+            }
         }
+
+        elevators.forEach(elevator => {
+            // Go to floor when button in elevator is pressed.
+            elevator.on('floor_button_pressed', (floorNum => {
+                if (!elevator.destinationQueue.includes(floorNum)) {
+                    elevator.destinationQueue.push(floorNum)
+                    elevator.checkDestinationQueue();
+                  }
+            }))
+
+            // Stop at floor if the passing floor is in the destinationQueue
+            elevator.on('passing_floor', (floorNum, direction) => {
+                if(elevator.destinationQueue.includes(floorNum)) {
+                    const index = elevator.destinationQueue.indexOf(floorNum)
+                    elevator.destinationQueue.splice(index, 1);
+                    elevator.destinationQueue.unshift(floorNum)
+                    elevator.checkDestinationQueue();
+                }
+            })
+        })
+
+        // Call elevator to floor
+        floors.forEach(floor => {
+            floor.on('down_button_pressed', () => {
+                callElevator(floor)
+            })
+
+            floor.on('up_button_pressed', () => {
+                callElevator(floor)
+            })
+        })
     },
 
     update: 
--- types/floor.ts
+++ types/floor.ts
@@ -10,6 +10,11 @@ interface Floor {
     buttonStates: { up: 'activated' | '', down: 'activated' | '' }
 
     /**
+     * The number of the floor
+     */
+    level: number
+
+    /**
      * Triggered when someone has pressed the up or down button at a floor. Note that
      * passengers will press the button again if they fail to enter an elevator
      */
