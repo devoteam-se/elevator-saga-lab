195cb1c3594bae0cfb085c4ba3af1501735c2b51
Filip Bontin <filip.bontin@devoteam.com>
2023-02-10 14:26:38 +0100

    Level 11 (and 12, 15, 16)

--- elevatorsaga.js
+++ elevatorsaga.js
@@ -7,11 +7,30 @@
         function init(elevators, floors) {
             for (const elevator of elevators) {
                 elevator.on('idle', () => {
-                    elevator.getPressedFloors().forEach(f => elevator.goToFloor(f));
-                    if (elevator.getPressedFloors().length === 0) {
-                        elevator.goToFloor(0);
-                    }
+                    elevator.goToFloor(0);
                 })
+                elevator.on('floor_button_pressed', (floorNum) => {
+                    elevator.goToFloor(floorNum);
+                })
+            }
+
+            const getEmptiestElevator = () => {
+                return [...elevators].sort((e1, e2) => {
+                    return e1.destinationQueue.length - e2.destinationQueue.length;
+                })[0];
+            }
+            const onFloorButtonPressed = (floor) => {
+                const elevatorsGoingThere = elevators
+                    .filter(e => e.destinationQueue.includes(floor.floorNum()))
+                    .filter(e => e.loadFactor() === 0)
+                if (!elevatorsGoingThere.length) {
+                    getEmptiestElevator().goToFloor(floor.floorNum());
+                }
+            }
+
+            for (const floor of floors) {
+                floor.on('up_button_pressed', onFloorButtonPressed);
+                floor.on('down_button_pressed', onFloorButtonPressed);
             }
         },
 
@@ -25,3 +44,4 @@
             // We normally don't need to do anything here
         }
 })
+
