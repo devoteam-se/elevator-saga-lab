81adaef3e4b8cc7fb20c1277077599cbefab58d5
Fredrik Jönsson <fredrik.jonsson@devoteam.com>
2023-02-10 09:31:04 +0100

    challenge1

--- elevatorsaga.js
+++ elevatorsaga.js
@@ -1,27 +1,26 @@
 ({
-    init:
+  init:
     /**
      * @param {Elevator[]} elevators - the elevators
      * @param {Floor[]} floors - the floors
      */
     function init(elevators, floors) {
-        // Let's go through all the elevators
-        for (const elevator of elevators) {
-            // Whenever the elevator is idle, make it go to all the floors
-            elevator.on('idle', () => {
-                elevator.goToFloor(0);
-                elevator.goToFloor(2);
-            })            
-        }
+      // Let's go through all the elevators
+      for (const elevator of elevators) {
+        // Whenever the elevator is idle, make it go to all the floors
+        elevator.on("idle", () => {
+          elevator.goToFloor(Math.floor(Math.random() * 3));
+        });
+      }
     },
 
-    update: 
+  update:
     /**
      * @param {number} dt - time difference from last call
      * @param {Elevator[]} elevators - the elevators
      * @param {Floor[]} floors - the floors
      */
     function update(dt, elevators, floors) {
-        // We normally don't need to do anything here
-    }
-})
+      // We normally don't need to do anything here
+    },
+});
